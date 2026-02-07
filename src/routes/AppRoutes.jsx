34c6f64b24c02a1e0a52b77776eb56d9b091ec72
jason <jaml08@hotmail.com>
2026-02-07 10:02:33 -0500

    feat: Agregue rutas privadas y publicas

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -5,17 +5,38 @@ import { Inventario } from "../pages/private/Inventario";
 import { Ventas } from "../pages/private/Ventas";
 import { Clientes } from "../pages/private/Clientes";
 import { Reportes } from "../pages/private/Reportes";
+import { Register } from "../pages/Register";
+import PrivateRoutes from "./PrivateRoutes";
+import PublicRoutes from "./PublicRoutes";
 
 export const AppRoutes = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/inventario" element={<Inventario />} />
-        <Route path="/ventas" element={<Ventas />} />
-        <Route path="/clientes" element={<Clientes />} />
-        <Route path="/reportes" element={<Reportes />} />
+        <Route
+          path="/"
+          element={
+            <PublicRoutes>
+              <Login />
+            </PublicRoutes>
+          }
+        />
+        <Route
+          path="/register"
+          element={
+            <PublicRoutes>
+              <Register />
+            </PublicRoutes>
+          }
+        />
+
+        <Route element={<PrivateRoutes />}>
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/inventario" element={<Inventario />} />
+          <Route path="/ventas" element={<Ventas />} />
+          <Route path="/clientes" element={<Clientes />} />
+          <Route path="/reportes" element={<Reportes />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
