377eb189c1f9448dc83ac4b2eb5c9ab14e2fca11
jason <jaml08@hotmail.com>
2026-02-17 02:24:24 -0500

    feat: componente loader

---
+++ src/components/loader/Spinner.jsx
@@ -0,0 +1,7 @@
+export const Spinner = () => {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <span className="loading loading-spinner loading-lg w-50 h-50"></span>
+    </div>
+  );
+};
