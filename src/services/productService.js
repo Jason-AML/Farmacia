5a8e18acac28bb1f514ef932b4b3501baffd578b
jason <jaml08@hotmail.com>
2026-03-18 02:52:08 -0500

    delete: funcion que actualizaba el stock

--- src/services/productService.js
+++ src/services/productService.js
@@ -77,15 +77,6 @@ export const getCategoria = async () => {
   }
   return data;
 };
-export const actualizarStock = async (carrito) => {
-  const updates = carrito.map((item) =>
-    supabase.rpc("decrementar_stock", {
-      p_producto_id: item.id,
-      p_cantidad: item.cantidad,
-    }),
-  );
-  await Promise.all(updates);
-};
 
 export const añadirStock = async (id, cantidad) => {
   const { data, error } = await supabase
