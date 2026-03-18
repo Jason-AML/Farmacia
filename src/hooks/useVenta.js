1a2fd9fa4015b06eedaed76f8eb5a77befdeb3f1
jason <jaml08@hotmail.com>
2026-03-18 01:46:40 -0500

    feat: agregar hooks useVenta, useSumaVenta y useSumaVentaMes

---
+++ src/hooks/useVenta.js
@@ -0,0 +1,29 @@
+import { useQuery } from "@tanstack/react-query";
+import { getVenta, sumaVentaHoy, sumaVentaMes } from "../services/ventaService";
+
+export const useVenta = () => {
+  const { data: venta = [], isLoading: loadingVenta } = useQuery({
+    queryKey: ["ventas"],
+    queryFn: getVenta,
+  });
+
+  return { venta, loadingVenta };
+};
+
+export const useSumaVenta = () => {
+  const { data: sumaVentas, isLoading: loadingSumaVenta } = useQuery({
+    queryKey: ["sumaVentas"],
+    queryFn: sumaVentaHoy,
+  });
+
+  return { sumaVentas, loadingSumaVenta };
+};
+
+export const useSumaVentaMes = () => {
+  const { data: sumaVentasMes, isLoading: loadingSumaVentaMes } = useQuery({
+    queryKey: ["sumaVentasMes"],
+    queryFn: sumaVentaMes,
+  });
+
+  return { sumaVentasMes, loadingSumaVentaMes };
+};
